8fe29d126ddb4f2ec75a5e4ae9e43737b22497cf
REBECA LOPES DE OLIVEIRA <21276568@CFP127ALUNO.LOCAL>
2021-09-03 15:55:23 -0300

    atividade finalizada

---
+++ src/multiplosquatro/MultiploDeQuatro.java
@@ -0,0 +1,23 @@
+package multiplosquatro;
+
+public class MultiploDeQuatro {
+
+	public static void main(String[] args) {
+
+		int numeroRepeticao = 100;
+		int contador = 0;
+		
+		while (contador <= numeroRepeticao) {
+			
+			contador = contador + 1;
+			if (contador % 4 == 0) {
+				System.out.println("Multiplos de 4: " + contador  );
+				
+			} 
+		
+
+	}
+
+}
+
+}
